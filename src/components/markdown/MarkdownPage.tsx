7d0b81b18e7f02407d2bb538ad024d7b0d60ccf5
Axelle Mentières <Mtieres@gmail.com>
2026-05-25 14:50:13 +0200

    refactor: simplify MarkdownPage component by removing unused className prop

--- src/components/markdown/MarkdownPage.tsx
+++ src/components/markdown/MarkdownPage.tsx
@@ -2,13 +2,8 @@ import ReactMarkdown from "react-markdown";
 
 interface MarkdownPageProps {
   content: string;
-  className?: string;
 }
 
-export function MarkdownPage({ content, className }: MarkdownPageProps) {
-  return (
-    <div className={`prose${className ? ` ${className}` : ""}`}>
-      <ReactMarkdown>{content}</ReactMarkdown>
-    </div>
-  );
+export function MarkdownPage({ content }: MarkdownPageProps) {
+  return <ReactMarkdown>{content}</ReactMarkdown>;
 }
