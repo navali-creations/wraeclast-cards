f21e93ebd8a0a142fca71e01f0d9999686ed5d4e
Sebastian Krzyżanowski <eskrzy@gmail.com>
2026-07-11 21:22:07 +0200

    feat: telemetry

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,31 @@
+import * as Sentry from "@sentry/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./app/App";
 import "./index.css";
+
+const sentryDsn = import.meta.env.VITE_SENTRY_DSN;
+const umamiScriptUrl = import.meta.env.VITE_UMAMI_SCRIPT_URL;
+const umamiWebsiteId = import.meta.env.VITE_UMAMI_WEBSITE_ID;
+const enableTelemetry = import.meta.env.PROD;
+
+if (enableTelemetry && sentryDsn) {
+  Sentry.init({
+    dsn: sentryDsn,
+    dataCollection: {
+      userInfo: false,
+      httpBodies: [],
+    },
+  });
+}
+
+if (enableTelemetry && umamiScriptUrl && umamiWebsiteId) {
+  const script = document.createElement("script");
+  script.defer = true;
+  script.src = umamiScriptUrl;
+  script.dataset.websiteId = umamiWebsiteId;
+  document.head.append(script);
+}
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
